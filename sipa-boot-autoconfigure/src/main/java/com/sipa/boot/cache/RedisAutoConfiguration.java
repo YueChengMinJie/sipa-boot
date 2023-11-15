395385c609ccb7243575e3048b4b9995320887c6
caszhou <zhouxiajie@qq.com>
2023-11-15 14:48:44 +0800

    feat(project): first init.

--- sipa-boot-autoconfigure/src/main/java/com/sipa/boot/cache/RedisAutoConfiguration.java
+++ sipa-boot-autoconfigure/src/main/java/com/sipa/boot/cache/RedisAutoConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.sipa.boot.cache.util.RedisUtil;
@@ -19,5 +23,15 @@ public class RedisAutoConfiguration {
     @Bean
     public StringRedisSerializer stringRedisSerializer() {
         return new StringRedisSerializer();
+    }
+
+    @Bean
+    @Primary
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        if (redisConnectionFactory instanceof LettuceConnectionFactory) {
+            LettuceConnectionFactory lcf = (LettuceConnectionFactory)redisConnectionFactory;
+            lcf.setValidateConnection(true);
+        }
+        return new StringRedisTemplate(redisConnectionFactory);
     }
 }
