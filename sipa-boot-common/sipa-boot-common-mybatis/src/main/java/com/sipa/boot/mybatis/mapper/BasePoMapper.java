2acf6feb8c5521a75d2bfbf2c9614f6aa7b139eb
caszhou <zhouxiajie@qq.com>
2024-03-12 16:21:33 +0800

    feature(工程): base po.

---
+++ sipa-boot-common/sipa-boot-common-mybatis/src/main/java/com/sipa/boot/mybatis/mapper/BasePoMapper.java
@@ -0,0 +1,12 @@
+package com.sipa.boot.mybatis.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sipa.boot.core.pojo.po.BasePo;
+
+/**
+ * @author caszhou
+ * @date 2024/3/12
+ */
+public interface BasePoMapper extends BaseMapper<BasePo> {
+    //
+}
