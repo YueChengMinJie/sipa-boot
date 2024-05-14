755a4f1e9ad853f1c338ce94d0dab11b31b74dc6
djmilk <dingjie@iezu.com.cn>
2024-05-14 19:19:47 +0800

    feat(device data):station mqtt/start work/warning msg into db

--- sipa-boot-common/sipa-boot-common-iot/src/main/java/com/sipa/boot/iot/protocol/mqtt/MqttProtocol.java
+++ sipa-boot-common/sipa-boot-common-iot/src/main/java/com/sipa/boot/iot/protocol/mqtt/MqttProtocol.java
@@ -5,6 +5,8 @@ package com.sipa.boot.iot.protocol.mqtt;
  * @date 2022/7/16
  */
 public interface MqttProtocol {
+
+    public static String DATA_FORMAT = "yyyy-MM-dd hh:mm:ss";
     MqttHeader getHeader();
 
     Object getDataBody();
