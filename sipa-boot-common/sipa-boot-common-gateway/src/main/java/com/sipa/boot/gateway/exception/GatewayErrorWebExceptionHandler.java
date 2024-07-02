4c3e7c8f51d6b3d41533f149f18610958636633e
caszhou <zhouxiajie@qq.com>
2024-07-02 09:00:11 +0800

    feature(1.1.3.2): 变量名rename.

--- sipa-boot-common/sipa-boot-common-gateway/src/main/java/com/sipa/boot/gateway/exception/GatewayErrorWebExceptionHandler.java
+++ sipa-boot-common/sipa-boot-common-gateway/src/main/java/com/sipa/boot/gateway/exception/GatewayErrorWebExceptionHandler.java
@@ -2,7 +2,6 @@ package com.sipa.boot.gateway.exception;
 
 import java.util.Map;
 
-import com.sipa.boot.core.exception.system.ESystemErrorCode;
 import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.boot.autoconfigure.web.reactive.error.DefaultErrorWebExceptionHandler;
@@ -13,6 +12,7 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.sipa.boot.core.exception.system.ESystemErrorCode;
 import com.sipa.boot.core.response.ResponseWrapper;
 
 import cn.hutool.http.HttpStatus;
@@ -41,31 +41,31 @@ public class GatewayErrorWebExceptionHandler extends DefaultErrorWebExceptionHan
 
     private static Mono<ServerResponse> getServerResponseMono(int status, Map<String, Object> error) {
         if (status == HttpStatus.HTTP_NOT_FOUND) {
-            ESystemErrorCode notFound = ESystemErrorCode.NOT_FOUND;
-            return doGetServerResponseMono(status, notFound, error);
+            ESystemErrorCode code = ESystemErrorCode.NOT_FOUND;
+            return doGetServerResponseMono(status, code, error);
         } else if (status == HttpStatus.HTTP_INTERNAL_ERROR) {
-            ESystemErrorCode notFound = ESystemErrorCode.GATEWAY_ERROR;
-            return doGetServerResponseMono(status, notFound, error);
+            ESystemErrorCode code = ESystemErrorCode.GATEWAY_ERROR;
+            return doGetServerResponseMono(status, code, error);
         } else if (status == HttpStatus.HTTP_BAD_GATEWAY) {
-            ESystemErrorCode notFound = ESystemErrorCode.BAD_GATEWAY;
-            return doGetServerResponseMono(status, notFound, error);
+            ESystemErrorCode code = ESystemErrorCode.BAD_GATEWAY;
+            return doGetServerResponseMono(status, code, error);
         } else if (status == HttpStatus.HTTP_UNAVAILABLE) {
-            ESystemErrorCode notFound = ESystemErrorCode.UNAVAILABLE;
-            return doGetServerResponseMono(status, notFound, error);
+            ESystemErrorCode code = ESystemErrorCode.UNAVAILABLE;
+            return doGetServerResponseMono(status, code, error);
         } else if (status == HttpStatus.HTTP_GATEWAY_TIMEOUT) {
-            ESystemErrorCode notFound = ESystemErrorCode.GATEWAY_TIMEOUT;
-            return doGetServerResponseMono(status, notFound, error);
+            ESystemErrorCode code = ESystemErrorCode.GATEWAY_TIMEOUT;
+            return doGetServerResponseMono(status, code, error);
         }
         return ServerResponse.status(status)
             .contentType(MediaType.APPLICATION_JSON)
             .body(BodyInserters.fromValue(error));
     }
 
-    private static Mono<ServerResponse> doGetServerResponseMono(int status, ESystemErrorCode notFound,
+    private static Mono<ServerResponse> doGetServerResponseMono(int status, ESystemErrorCode code,
         Map<String, Object> error) {
         log.info(JSONUtil.toJsonStr(error));
         return ServerResponse.status(status)
             .contentType(MediaType.APPLICATION_JSON)
-            .body(BodyInserters.fromValue(ResponseWrapper.error(notFound.getAllCode(), notFound.getMsg())));
+            .body(BodyInserters.fromValue(ResponseWrapper.error(code.getAllCode(), code.getMsg())));
     }
 }
