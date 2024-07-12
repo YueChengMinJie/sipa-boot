7f3d8500c4fb9feed8db51b58ac82915998ad650
caszhou <zhouxiajie@qq.com>
2024-07-12 15:15:28 +0800

    feature(init): first init.

--- sipa-boot-common/sipa-boot-common-test-container/src/main/java/com/sipa/boot/testcontainer/command/GuideCmd.java
+++ sipa-boot-common/sipa-boot-common-test-container/src/main/java/com/sipa/boot/testcontainer/command/GuideCmd.java
@@ -28,12 +28,12 @@ public class GuideCmd extends AbstractCommand {
             case GUIDE_HELP:
                 System.out.println("************** 欢迎使用轻量级TDD测试工具 ***************************");
                 System.out.println("**** 1.测试单个方法，请在控制台输入方法全称");
-                System.out.println("**** 例如：com.hmev.bcp.process.it.activiti.ProcessContractTest.testDeployment()");
+                System.out.println("**** 例如：com.sipa.bcp.process.it.activiti.ProcessContractTest.testDeployment()");
                 System.out.println("**** 2.测试整个测试类，请在控制台输入类全称");
-                System.out.println("**** 例如：com.hmev.bcp.process.it.activiti.ProcessContractTest");
+                System.out.println("**** 例如：com.sipa.bcp.process.it.activiti.ProcessContractTest");
                 System.out.println("**** 3.重复上一次测试，只需在控制台输入字母 - ‘r’");
                 System.out.println("**** 4.自动生成测试类,请输入‘new 方法全称  参数1 参数2 ...’");
-                System.out.println("**** 例如：new com.hmev.bcp.process.it.activiti.ProcessContractTest#testDeployment");
+                System.out.println("**** 例如：new com.sipa.bcp.process.it.activiti.ProcessContractTest#testDeployment");
                 System.out
                     .println("***********************************************************************************");
                 break;
