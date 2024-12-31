58c6b97a98cfa97b1b775a7968306ee44ff80b84
caszhou <zhouxiajie@qq.com>
2024-12-31 15:15:28 +0800

    feature(sentinel): 网关集成.

--- sipa-boot-autoconfigure/src/main/java/com/sipa/boot/sentinel/SentinelGatewayAutoConfiguration.java
+++ sipa-boot-autoconfigure/src/main/java/com/sipa/boot/sentinel/SentinelGatewayAutoConfiguration.java
@@ -1,10 +1,13 @@
 package com.sipa.boot.sentinel;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.init.InitExecutor;
 import com.sipa.boot.sentinel.gateway.SentinelGatewayEnvProcessor;
 import com.sipa.boot.sentinel.gateway.SipaBlockRequestHandler;
 
@@ -15,6 +18,11 @@ import com.sipa.boot.sentinel.gateway.SipaBlockRequestHandler;
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(SentinelGatewayEnvProcessor.class)
 public class SentinelGatewayAutoConfiguration {
+    @PostConstruct
+    private void init() {
+        InitExecutor.doInit();
+    }
+
     @Bean
     public BlockRequestHandler sipaBlockRequestHandler() {
         return new SipaBlockRequestHandler();
