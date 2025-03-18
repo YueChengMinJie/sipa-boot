45f9d4a419ca719ebe8e96994628302aef85a1af
caszhou <zhouxiajie@qq.com>
2025-03-18 17:01:59 +0800

    feature(数采): aoi.

--- sipa-boot-autoconfigure/src/main/java/com/sipa/boot/secure/SecureServerAutoConfiguration.java
+++ sipa-boot-autoconfigure/src/main/java/com/sipa/boot/secure/SecureServerAutoConfiguration.java
@@ -1,27 +1,17 @@
 package com.sipa.boot.secure;
 
 import javax.servlet.Filter;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.HttpHeaders;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-import com.sipa.boot.core.constant.SipaConstant;
-import com.sipa.boot.core.env.EnvConstant;
 import com.sipa.boot.secure.server.IdpUserHoldFilter;
 import com.sipa.boot.secure.server.IdpUserInterceptor;
 import com.sipa.boot.secure.server.SameTokenInterceptor;
 import com.sipa.boot.secure.server.property.SecureServerProperty;
 
-import cn.dev33.satoken.context.SaHolder;
-import cn.dev33.satoken.filter.SaServletFilter;
-import cn.dev33.satoken.same.SaSameUtil;
-import cn.dev33.satoken.spring.SpringMVCUtil;
-import cn.hutool.json.JSONUtil;
 import feign.RequestInterceptor;
 import lombok.RequiredArgsConstructor;
 
@@ -43,35 +33,35 @@ public class SecureServerAutoConfiguration {
     public static class FeignSecureServerAutoConfiguration {
         private final SecureServerProperty secureServerProperty;
 
-        @Bean
-        @Profile(EnvConstant.NOT_ENV_LOCAL)
-        public SaServletFilter saServletFilter() {
-            String authPattern = this.secureServerProperty.getAuthPattern();
-            String[] springDocPattern = this.secureServerProperty.getSpringDocPattern();
-
-            return new SaServletFilter()
-
-                .addInclude(authPattern)
-                .addExclude(springDocPattern)
-
-                .setAuth(obj -> {
-                    String token = SaHolder.getRequest().getHeader(SaSameUtil.SAME_TOKEN);
-                    SaSameUtil.checkToken(token);
-                })
-
-                .setError(e -> {
-                    HttpServletResponse response = SpringMVCUtil.getResponse();
-                    response.setStatus(SipaSecureUtil.getStatusCode(e));
-                    response.setHeader(HttpHeaders.CONTENT_TYPE, SipaConstant.HttpHeaderValue.JSON_UTF8);
-                    return JSONUtil.toJsonStr(SipaSecureUtil.handlerException(e));
-                });
-        }
-
-        @Bean
-        @Profile(EnvConstant.NOT_ENV_LOCAL)
-        public RequestInterceptor sameTokenInterceptor() {
-            return new SameTokenInterceptor();
-        }
+        // @Bean
+        // @Profile(EnvConstant.NOT_ENV_LOCAL)
+        // public SaServletFilter saServletFilter() {
+        // String authPattern = this.secureServerProperty.getAuthPattern();
+        // String[] springDocPattern = this.secureServerProperty.getSpringDocPattern();
+        //
+        // return new SaServletFilter()
+        //
+        // .addInclude(authPattern)
+        // .addExclude(springDocPattern)
+        //
+        // .setAuth(obj -> {
+        // String token = SaHolder.getRequest().getHeader(SaSameUtil.SAME_TOKEN);
+        // SaSameUtil.checkToken(token);
+        // })
+        //
+        // .setError(e -> {
+        // HttpServletResponse response = SpringMVCUtil.getResponse();
+        // response.setStatus(SipaSecureUtil.getStatusCode(e));
+        // response.setHeader(HttpHeaders.CONTENT_TYPE, SipaConstant.HttpHeaderValue.JSON_UTF8);
+        // return JSONUtil.toJsonStr(SipaSecureUtil.handlerException(e));
+        // });
+        // }
+        //
+        // @Bean
+        // @Profile(EnvConstant.NOT_ENV_LOCAL)
+        // public RequestInterceptor sameTokenInterceptor() {
+        // return new SameTokenInterceptor();
+        // }
 
         @Bean
         public RequestInterceptor idpUserInterceptor() {
